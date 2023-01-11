ebe9cd1042bca5e109758764516df3b67869cf1a
zedin09 <zgarzon@unal.edu.co>
2023-01-11 18:23:55 -0500

    API inicial funcionando

---
+++ main.py
@@ -0,0 +1,14 @@
+from typing import Union
+from fastapi import FastAPI
+
+# Creación de una aplicación FastAPI
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/hola")
+def read_root():
+    return {"Hola": "Mundo"}
