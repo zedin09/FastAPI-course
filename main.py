81d85c1a77e2cb094e748e55652f3574c66f2157
zedin09 <zgarzon@unal.edu.co>
2023-01-12 09:10:04 -0500

    Creación de UPDATE path for item

--- main.py
+++ main.py
@@ -26,3 +26,7 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.get("/calculadora")
 def calcular(operando1: float, operando2: float):
     return {"suma": operando1 + operando2}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
