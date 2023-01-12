46f9b18041ee97e139a452689970b8d8e0cf739c
zedin09 <zgarzon@unal.edu.co>
2023-01-12 08:57:50 -0500

    Creación del modelo Item con pydantic

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 # Creación de una aplicación FastAPI
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
 
 @app.get("/")
 def read_root():
@@ -12,3 +17,12 @@ def read_root():
 @app.get("/hola")
 def read_root():
     return {"Hola": "Mundo"}
+
+#item_id is a path(/) parameter and q is a query(?) parameter
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/calculadora")
+def calcular(operando1: float, operando2: float):
+    return {"suma": operando1 + operando2}
