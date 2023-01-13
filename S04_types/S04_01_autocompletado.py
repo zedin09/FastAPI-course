029ceb9ebf30b1ad43dd348255acb755967973f4
zedin09 <zgarzon@unal.edu.co>
2023-01-13 08:29:30 -0500

    Data type error in get full name function

--- S04_types/S04_01_autocompletado.py
+++ S04_types/S04_01_autocompletado.py
@@ -1,2 +1,6 @@
 def get_full_name(first_name, last_name):
     full_name = first_name.title() + ' ' + last_name.title()
+    return full_name
+
+print(get_full_name('john', 'ortiz'))
+# print(get_full_name('jphn', 0)) #AttributeError
