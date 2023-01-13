809fadb1bbab12c90e3255eb112cb1cdfe4a241b
zedin09 <zgarzon@unal.edu.co>
2023-01-13 10:48:18 -0500

    tipado de diccionarios

--- S04_types/S04_01_autocompletado.py
+++ S04_types/S04_01_autocompletado.py
@@ -2,5 +2,7 @@ def get_full_name(first_name, last_name):
     full_name = first_name.title() + ' ' + last_name.title()
     return full_name
 
+#! sin tipado en los parámetros de la fn no hay autocompletado
+
 print(get_full_name('john', 'ortiz'))
 # print(get_full_name('jphn', 0)) #AttributeError
--- S04_types/S4_03_deteccion_errores.py
+++ S04_types/S4_03_deteccion_errores.py
@@ -1,5 +1,6 @@
 def get_name_with_age(name: str, age: int):
     name_with_age = name + ' is this old: ' + age
+    # ! en la línea superior el editor debía mostrar un error
     return name_with_age
 
 print(get_name_with_age('John', 20))
