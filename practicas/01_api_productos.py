9de607bf88a99e2f858cdebe119e8213c2f7507c
zedin09 <zgarzon@unal.edu.co>
2023-01-15 20:33:21 -0500

    Actualizar el producto por id

--- practicas/01_api_productos.py
+++ practicas/01_api_productos.py
@@ -19,7 +19,6 @@ def index():
     return{'message': "Bienvenidos a la api de productos"}
 
 #? CREATE
-
 @app.post('/producto')
 def crear_producto(producto: Producto):
     producto.id = str(uuid())
@@ -27,7 +26,6 @@ def crear_producto(producto: Producto):
     return {'message': "Producto creado satisfactoriamente"}
 
 #? READ
-
 @app.get('/producto')
 def obtener_productos():
     return productos
@@ -41,14 +39,30 @@ def obtener_producto_por_id(producto_id: str):
     
     raise HTTPException(status_code=404, detail=f"El producto con el id {producto_id} no fue encontrado")
 
-#? DELETE
+#? UPDATE
+@app.put("/products/{producto_id}")
+def actualizar_producto(producto_id: str, producto: Producto):
+    resultado = list(filter(lambda p: p.id == producto_id, productos))
+    
+    if len(resultado):
+        p_encontrado = productos[0]
+        p_encontrado.nombre = producto.nombre
+        p_encontrado.precio_compra = producto.precio_compra
+        p_encontrado.precio_venta = producto.precio_venta
+        p_encontrado.proveedor = producto.proveedor
+        
+        return p_encontrado
+    
+    raise HTTPException(status_code=404, detail=f"El producto con el id {producto_id} no fue encontrado")
+    
 
+#? DELETE
 @app.delete('/producto/{producto_id}')
 def eliminar_producto_por_id(producto_id: str):
     resultado = list(filter(lambda p: p.id == producto_id, productos))
     
     if len(resultado):
-        producto = producto[0]
+        producto = productos[0]
         productos.remove(producto)
         
         return {'message': f'El producto con el id {producto_id} fue eliminado'}
